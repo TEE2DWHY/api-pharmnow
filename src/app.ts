e15cfdf147ccab4a2479003c22e6a6a818307293
TEE2DWHY <isaacolorunfemi330@gmail.com>
2025-06-30 23:38:36 +0100

    updated app.ts

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 8000;
 
 const start = async () => {
   try {
-    await connect(process.env.MONGO_URI as string);
+    await connect(process.env.MONGODB_URI as string);
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
