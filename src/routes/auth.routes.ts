d606574b2e297e4698e525d3248b16b8326fc30d
TEE2DWHY <isaacolorunfemi330@gmail.com>
2025-07-02 23:57:48 +0100

    added the change password functionality to the auth routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,14 +1,19 @@
 import { Router } from "express";
 import * as authController from "../controllers/auth.controller";
+import authorization from "../middlewares/authorization.middlware";
 
 const router = Router();
 
-// Public authentication routes
+// Public routes
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.post("/verify-email", authController.verifyCode);
 router.post("/forgot-password", authController.forgotPassword);
 router.post("/reset-password", authController.resetPassword);
 router.post("/resend-verification", authController.resendVerificationCode);
+
+// Authenticated routes
+router.use(authorization);
+router.put("/change-password", authController.changePassword);
 
 export default router;
